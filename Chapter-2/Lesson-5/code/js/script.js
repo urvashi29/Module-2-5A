e103fadca4774d474edca58cc8d80724a43356ae
urvashi29 <urvashigolu29@gmailcom>
2025-11-08 15:47:28 +0530

    buddy session

--- Chapter-2/Lesson-5/code/js/script.js
+++
@@ -1,7 +0,0 @@
-// Event Propogation
-// Event Delegation
-// delete node
-// classList (add class to an element through js)
-// Text Content, innerHTML
-// e.preventDefault()
-// remove event from an element
