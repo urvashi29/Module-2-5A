3b2e4322262c8b7b0f3f95aa968cbaac02c8e5a2
urvashi29 <urvashigolu29@gmail.com>
2025-12-12 20:49:14 +0530

    added api call in Chapter-3/l12

--- Chapter-3/Lesson-12/code/src/App.jsx
+++ Chapter-3/Lesson-12/code/src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Counter from "./pages/Counter";
+import Product from "./pages/Products";
 
 const App = () => {
   return (
     <>
       <Counter />
+      <Product />
     </>
   );
 };
